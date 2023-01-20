a1f87d48b458e39a8c51671409d9138ce49e69ca
Lemmy731 <lemmy.osiki@decagon.dev>
2023-01-20 09:41:44 +0100

    clone develop

--- HotelManagement.Api/Controllers/UpdateUserAppController.cs
+++ HotelManagement.Api/Controllers/UpdateUserAppController.cs
@@ -1,26 +1,26 @@
-﻿using HotelManagement.Core.DTOs;
-using HotelManagement.Core.IServices;
-using HotelManagement.Services.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using HotelManagement.Core.DTOs;
+//using HotelManagement.Core.IServices;
+//using HotelManagement.Services.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace HotelManagement.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UpdateUserAppController : ControllerBase
-    {
-        private readonly IUpdateUserAppService _userService;
+//namespace HotelManagement.Api.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class UpdateUserAppController : ControllerBase
+//    {
+//        private readonly IUpdateUserAppService _userService;
 
-        public UpdateUserAppController(IUpdateUserAppService userService)
-        {
-            _userService = userService;
-        }
-        [HttpPut]
-        public async Task<IActionResult> UpdateById(UpdateAppUserDto update, string Id)
-        {
-            var user = await _userService.UpdateUser(update, Id);
-            return Ok(user);
-        }
-    }
-}
+//        public UpdateUserAppController(IUpdateUserAppService userService)
+//        {
+//            _userService = userService;
+//        }
+//        [HttpPut]
+//        public async Task<IActionResult> UpdateById(UpdateAppUserDto update, string Id)
+//        {
+//            var user = await _userService.UpdateUser(update, Id);
+//            return Ok(user);
+//        }
+//    }
+//}
